ffe970b8cde0f2899e98965c6dcdb1e594f453c8
sarathm003 <sarathmk003@gmail.com>
2024-07-25 19:46:51 +0530

    commit

---
+++ sub.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class sub{
+public static void main (String[]args){
+Scanner scan=new Scanner(System.in);
+int num1=scan.nextInt();
+int num2=scan.nextInt();
+int sum=num1-num2;
+System.out.println("the subtraction is "+sum);
+}
+}
