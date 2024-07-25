2d9cf95ed395132c8b01e7e39f89b7139a5a83ad
sarathm003 <sarathmk003@gmail.com>
2024-07-25 19:17:22 +0530

    commit

---
+++ Input.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Input{
+public static void main (String[]args){
+Scanner obj=new Scanner(System.in);
+int number=obj.nextInt();
+System.out.println("the given number is "+number);
+}
+}
